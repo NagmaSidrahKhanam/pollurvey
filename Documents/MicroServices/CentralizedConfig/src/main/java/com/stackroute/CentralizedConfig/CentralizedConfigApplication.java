3c47fc0f00ec5203da68f57f86616c8f324bf8c0
khsidra <nagma.sidra@gmail.com>
2019-07-31 15:39:43 +0530

    first commit

---
+++ Documents/MicroServices/CentralizedConfig/src/main/java/com/stackroute/CentralizedConfig/CentralizedConfigApplication.java
@@ -0,0 +1,14 @@
+package com.stackroute.CentralizedConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class CentralizedConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralizedConfigApplication.class, args);
+	}
+
+}
